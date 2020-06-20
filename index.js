eaf7b5b35dc0ea21877a3e2233f9b0d902036ee4
Kamil Jaworski <jaworskiebiznes@gmail.com>
2020-06-20 14:28:39 +0200

    add footer, add articles

---
+++ index.js
@@ -0,0 +1,58 @@
+// Elements
+const arrow = document.querySelector(".arrow-top"); 
+
+// Functions
+setTimeout(clientCarousel(), 2000)
+arrowTop();
+
+
+
+function clientCarousel() {
+    let isShow = true;
+    const clientBox1 = document.querySelector('.ccb-1')
+    const clientBox2 = document.querySelector('.ccb-2')
+    const clientInputs = document.querySelectorAll(".client-carousel--inp");
+    const clientCarousel = document.getElementById("client-row")
+
+    const boxShow = (arg) => {
+        if(arg) {
+            clientBox1.style.opacity = "0";
+            clientBox2.style.opacity = "1";
+            clientInputs[0].style.background = "transparent"
+            clientInputs[1].style.background = "white"  
+        } else {
+            clientBox1.style.opacity = "1";
+            clientBox2.style.opacity = "0";
+            clientInputs[1].style.background = "transparent"
+            clientInputs[0].style.background = "white"
+        }
+        return isShow = !isShow
+    }
+    let myInt = setInterval(() => boxShow(isShow), 10000)
+
+    clientCarousel.addEventListener("mouseover", () => {
+        clearInterval(myInt)
+        console.log("dziala")
+    })
+    clientCarousel.addEventListener("mouseout", () => {
+        myInt = setInterval(() => boxShow(isShow), 10000)
+    })
+
+    clientInputs[0].addEventListener("click", () => boxShow(isShow));
+    clientInputs[1].addEventListener("click", () => boxShow(isShow));
+ 
+}
+
+function arrowTop() { 
+    window.addEventListener("scroll", (el) => {
+        if(window.scrollY > 300) {
+            arrow.style.opacity = "1"
+        } else {
+            arrow.style.opacity = "0"
+        }
+    })
+    arrow.addEventListener("click", () => {
+        window.scrollTo(0,0)
+    })
+}
+
